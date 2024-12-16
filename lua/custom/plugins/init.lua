016a954afaa29f21bbd8c2824d74d8d26c0eeaf1
Kelvin Wei <52874397+kelviy@users.noreply.github.com>
2024-12-16 22:10:25 +0200

    add auto save

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,13 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  -- autosave
+	"Pocco81/auto-save.nvim",
+	config = function()
+		 require("auto-save").setup {
+			-- your config goes here
+			-- or just leave it empty :)
+		 }
+	end,
+}
